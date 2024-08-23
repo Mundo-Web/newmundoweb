5362e3551bdf2350b3543ac0894881a3f9400dcb
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-08-23 11:44:11 -0500

    final

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -256,7 +256,7 @@
                             visitantes en clientes leales</p>
 
                         <div class="w-full text-sm lg:hidden">
-                            <form id="formHome">
+                            <form id="formHomemovil">
                                 @csrf
 
                                 <div class="grid grid-cols-12 mt-2">
@@ -630,6 +630,85 @@
         //     });
         //   }
         // });
+        fetch("{{ route('save.crm') }}", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    _token: $('[name="_token"]').val(),
+                    contact_name: $('[name="contact_name"]').val(),
+                    contact_email: $('[name="contact_email"]').val(),
+                    contact_phone: `51${$('[name="contact_phone"]').val()}`,
+                    // tradename: $('[name="name"]').val(),
+                    // sector: $('[name="sector"]').val(),
+                    // web_url: $('[name="web_url"]').val(),
+                    source: $('[name="source"]').val(),
+                    origin: $('[name="source"]').val(),
+                    client_width: $('[name="client_width"]').val(),
+                    client_height: $('[name="client_height"]').val(),
+                    client_latitude: $('[name="client_latitude"]').val(),
+                    client_longitude: $('[name="client_longitude"]').val(),
+                    client_system: $('[name="client_system"]').val(),
+                    message: $('[name="message"]').val(),
+                    triggered_by: $('[name="triggered_by"]').val(),
+
+                })
+            })
+            .then(async res => {
+                const data = await res.json()
+                if (!res.ok) throw new Error(data?.message ?? 'Ocurrio un error inesperado')
+                return data
+            })
+            .then(response => {
+                Swal.close();
+                Swal.fire({
+                    title: response.message,
+                    icon: "success",
+                });
+
+                $('#formHome')[0].reset();
+
+                window.location.href = 'https://mundoweb.pe/#formularioListo';
+
+            }).catch((error) => {
+                Swal.close();
+                Swal.fire({
+                    title: error,
+                    icon: "error",
+                });
+            })
+
+    })
+</script>
+
+
+<script>
+    $('#formHomemovil').submit(function(event) {
+
+        event.preventDefault();
+        Swal.fire({
+
+            title: 'Procesando información',
+            html: `Enviando... 
+      <div class="max-w-2xl mx-auto overflow-hidden flex justify-center items-center mt-4 ">
+        <div role="status">
+            <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+            </svg>
+            
+        </div>
+      </div>
+      `,
+            allowOutsideClick: false,
+            onBeforeOpen: () => {
+                Swal.showLoading();
+            }
+        });
+
+        let formDataArray = $(this).serializeArray();
+
         fetch("{{ route('save.crm') }}", {
                 method: 'POST',
                 headers: {
